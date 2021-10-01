efb5c55a0b51461906e8ff23d729bd550fe7f597
stav1997 <stav1997s@gmail.com>
2021-10-01 14:47:43 +0300

    all functionality

---
+++ Orders/order.model.js
@@ -0,0 +1,34 @@
+const conn = require('../server/database/connection');
+
+var Order = function(order){
+    this.clientName = order.clientName;
+    this.clientPhone = order.clientPhone;
+    this.orderDetails = order.orderDetails;
+    this.orderNote = order.orderNote;
+};
+
+Order.create = function (newOrder, result) {
+    conn.db.query("INSERT INTO orders set ?", newOrder, function (err, res) {
+        if(err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+        else{
+            result(null, res.insertId);
+        }
+    });
+};
+
+Order.findAll = function (result) {
+    conn.db.query("Select orderId, clientName, clientPhone, orderDetails, orderNote, TIME_FORMAT(orderDate, '%H:%i:%s') as 'orderTime' FROM orders WHERE  DATE_FORMAT(orderDate, '%Y-%m-%d') = SUBDATE(CURRENT_DATE, 1) ORDER BY orderDate DESC", function (err, res) {
+        if(err) {
+            console.log("error: ", err);
+            result(null, err);
+        }
+        else{
+            result(null, res);
+        }
+    });
+};
+
+module.exports = Order;
